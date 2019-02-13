16195ae6520e9a75f838d7deb25fea67c9780af1
Pierre Francois de Rouge <pifoupif@hotmail.com>
2019-02-13 16:05:33 +0100

    done finitooo

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,22 +2,27 @@ class ReviewsController < ApplicationController
 
   def create
     @review = Review.new(review_params)
+    @review.restaurant = Restaurant.find(params[:restaurant_id])
     if @review.save
-      redirect_to review_path(@review)
+      redirect_to restaurant_path(params[:restaurant_id])
     else
-      render 'new'
+      render :new
+    end
   end
 
   def new
+    @restaurant = Restaurant.find(params[:restaurant_id])
     @review = Review.new
   end
 
   def index
+    @restaurant = Restaurant.find(params[:restaurant_id])
     @reviews = Review.all
   end
 
   def show
-    @review = Review.find(params[:id])
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.find(params[:restaurant_id])
   end
 
 private
